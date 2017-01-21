b737a8ee73517dfb9bbffed8cee9bc8d5ddabaaa
Jordan <jordandlee@hotmail.ca>
2017-01-21 18:04:35 -0500

    Added comments

--- UTEK_2017/part1.py
+++ UTEK_2017/part1.py
@@ -2,9 +2,10 @@
 
 import urllib, json
 from haversine import haversine_func
-
+""" Class to represent a station """
 class Station:
 
+    """ Returns the max of 3 stations"""
     @staticmethod
     def getMax(station0, station1, station2):
         if station0.distance >= station1.distance and station0.distance >= station1.distance:
@@ -14,22 +15,39 @@ class Station:
         if station2.distance >= station0.distance and station2.distance >= station1.distance:
             return station2, 2
 
+    """
+    Constructor
 
+    Arguments:
+    latitude -- the latitude of the station
+    longitude -- the longitude of the station
+    name -- the name of the station
+    distance -- the distance from a given starting point to this station calculated with Haversine formula
+    """
     def __init__(self, latitude, longitude, name, distance):
         self.latitude = latitude
         self.longitude = longitude
         self.distance = distance
         self.name = name
 
+    """ Prints the station according to competition specifications"""
     def mPrint(self):
         mString = str(self.latitude) + " " + str(self.longitude) + " " + self.name + " " + str(self.distance)
         print mString
 
 
-# The function for part1. Takes coordinate as input, prints 3 closest stations as output
+"""
+Part 1 of UTEK
+
+Arguments:
+coordinate -- a string of the coordinate
+
+Output:
+Prints 3 closest stations as output
+"""
 def part1(coordinate):
 
-    # Parse the coordinate string
+    # Parse the coordinate string into floats
     given_lat, given_lng = map(float, coordinate.split(' '))
 
     # Make the request and get the JSON
@@ -41,8 +59,11 @@ def part1(coordinate):
     #print num_results
 
     list_of_stations = data["fuel_stations"]
+
+    # This list will contain the 3 minimum stations
     min_stations = []
 
+    # Loop through the list of stations
     for station_dict in list_of_stations:
 
         latitude = station_dict["latitude"]
@@ -52,20 +73,23 @@ def part1(coordinate):
 
         curr_station = Station(latitude, longitude, name, distance)
 
+        # If don't have 3 stations, append station
         if len(min_stations) < 3:
             min_stations.append(curr_station)
         else:
+            # Find the minimum station with the current max value and replace it if current station is closer
             max_station, index = Station.getMax(min_stations[0], min_stations[1], min_stations[2])
 
             if curr_station.distance < max_station.distance:
                 min_stations[index] = curr_station
 
 
-    # To sort the list in place by distance
+    # Sort the list in place by distance
     min_stations.sort(key=lambda x: x.distance, reverse=False)
 
     for station in min_stations:
         station.mPrint()
+
 
 part1('38.8977 -77.0365')
 
--- UTEK_2017/part4.py
+++ UTEK_2017/part4.py
@@ -1,23 +1,34 @@
 import os
-import googlemaps
 from part2 import get_distance_and_time
 import urllib, json
 from haversine import haversine_func
 
+"""
+Class to represent a Node in the graph
+"""
 class Node:
 
+    """ Constructor """
     def __init__(self, latitude, longitude, name, id):
         self.latitude = latitude
         self.longitude = longitude
-        #self.distance = distance
         self.name = name
         self.id = id
 
-
+    """ Prints according to competitions specification """
     def mPrint(self):
         mString = str(self.latitude) + " " + str(self.longitude) + " " + str(self.name) + " " + str(self.id)
         print (mString)
 
+    """
+    Function that takes a dictionary of points as input and returns a list of points that are within 480km
+
+    Arguments:
+    list_of_nodes -- the graph of nodes represented by a dictionary
+
+    Return:
+    closePointsList -- a list of points that are within 480km
+    """
     def close_points(self, list_of_nodes):
         # return list of nodes that are within 480km
         closePointsList = []
@@ -32,13 +43,17 @@ class Node:
 
         return closePointsList
 
+""" Class that represents the graph """
 class Graph:
 
+    """ Constructor that parses through JSON file to create the graph """
     def __init__(self):
 
+        # JSON parsing to get list of stations
         data = json.load(open("charging_stations.json"))
-
         list_of_stations = data["fuel_stations"]
+
+        # the actual graph
         self.list_of_nodes = {}
 
         for station_dict in list_of_stations:
@@ -47,12 +62,19 @@ class Graph:
             name = station_dict["station_name"]
             id = station_dict["id"]
 
+            # Put node into the dictionary
             self.list_of_nodes[id] = Node(latitude, longitude, name, id)
 
+        # Debugging purposes
         # for key in self.list_of_nodes:
         #      self.list_of_nodes[key].mPrint()
 
+    """
+    Function that takes a start and end point as input and returns the optimal path, time and distance
+    """
     def findPath(self, start, end):
+
+        # Create nodes for start and end
         currNode = Node(start[0], start[1], "Start", 0)
         endNode = Node(end[0], end[1], "End", 1)
 
