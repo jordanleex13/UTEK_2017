21ae2fbdae4ba3ba4779d0e329700d17239ee9a8
Javed Nissar <javednissar@gmail.com>
2017-01-21 14:14:14 -0500

    completed function for getting distance and time

--- part2.py
+++ part2.py
@@ -5,3 +5,13 @@ from tinydb import TinyDB, Query
 GMAPS = googlemaps.Client(key=os.environ['UTEK_2017_API_KEY'])
 db = TinyDB('db.json')
 
+def get_distance_and_time(point1, point2):
+    distance_matrix = GMAPS.distance_matrix(point1,point2)
+    element = distance_matrix["rows"][0]["elements"][0]
+    distance = element["distance"]["value"]
+    duration = element["duration"]["value"]
+    return distance/100,duration/(60*60)
+
+point1 = (37.773972, -122.431297)
+point2 = (37.4931367, -121.9453883)
+print(get_distance_and_time(point1, point2))
