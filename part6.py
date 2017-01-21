73c48b93300c74cf6928aaea6723858cbceb1f1e
Javed Nissar <javednissar@gmail.com>
2017-01-21 17:33:59 -0500

    implemented search

--- part6.py
+++ part6.py
@@ -1,9 +1,31 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request
+from geopy.geocoders import Nominatim
+from part4 import startPath
+
+geolocator = Nominatim()
 APP = Flask(__name__)
 
 @APP.route("/")
 def home():
     return render_template('home.html')
+
+@APP.route("/search")
+def search():
+    start_str = request.args["start"]
+    end_str = request.args["end"]
+    start_location = geolocator.geocode(start_str)
+    end_location = geolocator.geocode(end_str)
+    start_tuple = (start_location.latitude, start_location.longitude)
+    end_tuple = (end_location.latitude, end_location.longitude)
+    path = part4.startPath(start_tuple, end_tuple)
+    
+    name_of_path_points = [start_location.address]
+    for index,node in enumerate(path[1:]):
+        if index < len(path) - 1:
+            name_of_path_points.append(node.address)
+    name_of_path_points.append(end_location.address)
+
+    return render_template('search.html',path_names=name_of_path_points)
 
 if __name__ == "__main__":
     APP.run()
