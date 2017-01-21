6b39e83d931efa2525538b092fc56a8f48345bd8
Jordan <jordandlee@hotmail.ca>
2017-01-21 18:18:20 -0500

    Added comments throughout

--- UTEK_2017/part2.py
+++ UTEK_2017/part2.py
@@ -1,12 +1,20 @@
 import os
 import googlemaps
 
+# Google Maps API
 GMAPS = googlemaps.Client(key="AIzaSyBD9ncqo4K7G03DIF-TO-vaRBxq-FDzlyc")
 
-
+"""
+Takes two points and calculates the distance between the two and the time to get there
+Arguments:
+point1 -- the first point
+point2 -- the second point
+Return:
+distance -- the distance in kilometers
+duration -- the time in hours
+"""
 def get_distance_and_time(point1, point2):
-    print("HI")
-    distance_matrix = GMAPS.distance_matrix(point1,point2)
+    distance_matrix = GMAPS.distance_matrix(point1, point2)
     element = distance_matrix["rows"][0]["elements"][0]
     distance = element["distance"]["value"]
     duration = element["duration"]["value"]
